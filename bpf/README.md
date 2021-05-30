49713e732a43dfdf81f0c13ec09dd7b947b607cc
patrick <zipat@t-online.de>
2021-05-30 21:33:44 +0200

    Add bpf build system

---
+++ bpf/README.md
@@ -0,0 +1,5 @@
+```bash
+rm -rf ./build/ && mkdir -p ./build/
+cmake -B ./build/ -S .
+cmake --build ./build/
+```
---
+++ bpf/src/xdp_drop_kern.c
@@ -0,0 +1,11 @@
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+SEC("prog")
+int xdp_drop(struct xdp_md *ctx)
+{
+    return XDP_DROP;
+}
+
+extern char _license[];
+char _license[] SEC("license") = "GPL";
